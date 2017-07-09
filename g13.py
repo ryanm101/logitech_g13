35cbc1a90e2e73a015e59e925d984fc405c6064d
Ryan McLean <pvtryan100@gmail.com>
2017-07-09 22:24:56 +0100

    created class for g13 driver

--- g13.py
+++ g13.py
@@ -1,68 +1,60 @@
 import usb.core
 import usb.util
 import sys
- 
-# find our device
-dev = usb.core.find(idVendor=0x046d, idProduct=0xc21c)
- 
-# was it found?
-if dev is None:
-    raise ValueError('Device not found')
- 
-# Linux kernel sets up a device driver for USB device, which you have
-# to detach. Otherwise trying to interact with the device gives a
-# 'Resource Busy' error.
-try:
-    dev.detach_kernel_driver(0)
-except Exception, e:
-    pass # already unregistered
 
+class g13(object):
+    def __init__(self):
+        self.dev = None
+        self.interface = 0
+        self.getDevice(0x046d,0xc21c)
+        self.detachKernelDriver(self.dev)
+        # set the active configuration. With no arguments, the first
+        # configuration will be the active one
+        self.dev.set_configuration()
+        self.claimInterface(self.dev, self.interface)
+        self.endpoint = self.dev[0][(0,0)][0]
+        self.listenfordata(self.dev,self.endpoint)
 
-# set the active configuration. With no arguments, the first
-# configuration will be the active one
-dev.set_configuration()
- 
-# Let's fuzz around! 
- 
-# Lets start by Reading 1 byte from the Device using different Requests
-# bRequest is a byte so there are 255 different values
-#for bRequest in range(255):
-#    try:
-#        ret = dev.ctrl_transfer(0x80, bRequest, 0, 0, 1)
-#        print "bRequest ",bRequest
-#        print ret
-#    except:
-#        # failed to get data for this request
-#        pass
+    def getDevice(self, vendorid, prodid):
+        # find our device
+        self.dev = usb.core.find(idVendor=vendorid, idProduct=prodid)
+        if self.dev is None:
+            raise ValueError('Device not found')
 
-interface = 0
-endpoint = dev[0][(0,0)][0]
-usb.util.claim_interface(dev, interface)
+    def detachKernelDriver(self, dev):
+        # Linux kernel sets up a device driver for USB device, which you have
+        # to detach. Otherwise trying to interact with the device gives a
+        # 'Resource Busy' error.
+        try:
+            dev.detach_kernel_driver(0)
+        except Exception, e:
+            pass # already unregistered
 
-collected = 0
-attempts = 100
+    def claimInterface(self, dev, interface):
+        usb.util.claim_interface(dev, interface)
+
+    def listenfordata(self,dev,endpoint):
+        while(1):
+            try:
+                data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
+                print data
+            except usb.core.USBError as e:
+                data = None
+                if e.args == ('Operation timed out',):
+                    continue
+
+    def cleanup(self,dev, interface):
+        # release the device
+        usb.util.release_interface(dev, interface)
+        # reattach the device to the OS kernel
+        dev.attach_kernel_driver(interface)
+
 # byte 0 always =1
 # bytes 1,2 Joystick control
-# bytes 3,4,5 are GKeys 
+# bytes 3,4,5 are GKeys
 # byte 5 msb, 128 = LCD Light on, 0 = LCD Light off
 # byte 6 LCD Control Keys & M1-3
 # byte 7 Joystick Keys & MR Key
 #array('B', [1, 134, 130, 1, 0, 128, 0, 128])
-while collected < attempts :
-    try:
-        data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
-        collected += 1
-        print data
-        #if data[3] != 0 or data[4] != 0:
-        #    print "GKey 1-16 Pressed"
-    except usb.core.USBError as e:
-        data = None
-        if e.args == ('Operation timed out',):
-            continue
-
-# release the device
-usb.util.release_interface(dev, interface)
-# reattach the device to the OS kernel
-dev.attach_kernel_driver(interface)
-
-
+if __name__ == "__main__":
+    g = g13()
