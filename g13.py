5ae0245ea924bf4a5277c1f9420d3861bf36e095
Ryan McLean <pvtryan100@gmail.com>
2017-07-10 01:13:07 +0100

    Catch LCD Key presses

--- g13.py
+++ g13.py
@@ -6,7 +6,22 @@ class g13(object):
     def __init__(self):
         self.dev = None
         self.interface = 0
-        self.getDevice(0x046d,0xc21c)
+        self.vendorid = 0x046d
+        self.prodid = 0xc21c
+        self.endpoint = None
+        self.maskdic = {
+            "1": 1,
+            "2": 2,
+            "4": 3,
+            "8": 4,
+            "16": 5,
+            "32": 6,
+            "64": 7,
+            "128": 8
+        }
+
+    def setup(self):
+        self.getDevice(self.vendorid,self.prodid)
         self.detachKernelDriver(self.dev)
         # set the active configuration. With no arguments, the first
         # configuration will be the active one
@@ -32,6 +47,32 @@ class g13(object):
     def claimInterface(self, dev, interface):
         usb.util.claim_interface(dev, interface)
 
+    def getLCDKeys(self,byte):
+        matches = list()
+        binary = long(hex(byte),16)
+        for k in self.maskdic.keys():
+            mask = long(hex(int(k)),16)
+            if binary & mask != 0:
+                if int(k) > 31:
+                    continue
+                _str = "LCDK{}".format(self.maskdic[k])
+                matches.append(_str)
+        return matches
+
+    def getGKeys(self, bytelst):
+        matches = list()
+        for idx,byte in enumerate(bytelst):
+            _mod = idx * 8
+            binary = long(hex(byte),16)
+            for k in self.maskdic.keys():
+                mask = long(hex(int(k)),16)
+                if binary & mask != 0:
+                    if idx == 2 and int(k) > 63:
+                        continue
+                    _str = "G{}".format(self.maskdic[k] + _mod)
+                    matches.append(_str)
+        return matches
+
     def isLCDLightOn(self,byte):
         if long(hex(byte),16) & 0x80:
             return True
@@ -41,17 +82,34 @@ class g13(object):
         while(1):
             try:
                 data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
-                print data
+                #print data
+                print "[{0},{2},{4},{6},{8},{10},{12},{14}]::::[{1},{3},{5},{7},{9},{11},{13}]".format(
+                        hex(data[0]),data[0],
+                        hex(data[1]),data[1],
+                        hex(data[2]),data[2],
+                        hex(data[3]),data[3],
+                        hex(data[4]),data[4],
+                        hex(data[5]),data[5],
+                        hex(data[6]),data[6],
+                        hex(data[7]),data[7]
+                    )
+
             except usb.core.USBError as e:
                 data = None
                 if e.args == ('Operation timed out',):
                     continue
 
-    def cleanup(self,dev, interface):
+    def cleanup(self):
         # release the device
-        usb.util.release_interface(dev, interface)
+        try:
+            usb.util.release_interface(self.dev, self.interface)
+        except:
+            pass
         # reattach the device to the OS kernel
-        dev.attach_kernel_driver(interface)
+        try:
+            self.dev.attach_kernel_driver(self.interface)
+        except:
+            pass
 
 # byte 0 always =1
 # bytes 1,2 Joystick control (x,y) coordinates
@@ -62,4 +120,5 @@ class g13(object):
 #array('B', [1, 134, 130, 1, 0, 128, 0, 128])
 if __name__ == "__main__":
     g = g13()
+    g.setup()
     g.listenfordata(g.dev,g.endpoint)
--- test_g13.py
+++ test_g13.py
@@ -2,14 +2,88 @@
 
 from __future__ import print_function #py3 makes print a function print()
 import unittest
+import random
+from operator import add
 import g13
 
 class g13Test(unittest.TestCase):
     def setUp(self):
         self.g = g13.g13()
+        self.gk, self.lcdk = self.KeysLists()
 
     def tearDown(self):
-        self.g.cleanup(self.g.dev, self.g.interface)
+        self.g.cleanup()
+
+    def KeysLists(self):
+        _GkeyList = {
+            "G1": [1,0,0],
+            "G2": [2,0,0],
+            "G3": [4,0,0],
+            "G4": [8,0,0],
+            "G5": [16,0,0],
+            "G6": [32,0,0],
+            "G7": [64,0,0],
+            "G8": [128,0,0],
+            "G9": [0,1,0],
+            "G10": [0,2,0],
+            "G11": [0,4,0],
+            "G12": [0,8,0],
+            "G13": [0,16,0],
+            "G14": [0,32,0],
+            "G15": [0,64,0],
+            "G16": [0,128,0],
+            "G17": [0,0,1],
+            "G18": [0,0,2],
+            "G19": [0,0,4],
+            "G20": [0,0,8],
+            "G21": [0,0,16],
+            "G22": [0,0,32]
+        }
+        _LCDKeyList = {
+            "LCDK1": 1, # Selector
+            "LCDK2": 2,
+            "LCDK3": 4,
+            "LCDK4": 8,
+            "LCDK5": 16
+        }
+        return _GkeyList, _LCDKeyList
+
+    # Check Methods
+    def test_getGKeys_G1_22(self):
+        for i in range(1,22):
+            _Keystr = "G{}".format(i)
+            res = self.g.getGKeys(self.gk[_Keystr])
+            self.assertEqual(len(res), 1)
+            self.assertEqual(res[0], _Keystr)
+
+    def test_getGKeys_MultiKey(self):
+        _key1 = random.choice(self.gk.keys())
+        _key2 = None
+        while True:
+            _key2 = random.choice(self.gk.keys())
+            if _key1 != _key2:
+                break
+        _keys = map(add,self.gk[_key1],self.gk[_key2])
+        res = self.g.getGKeys(_keys)
+        self.assertIn(_key1, res)
+        self.assertIn(_key2, res)
+
+    def test_getGKeys_NonExistant_andG1(self):
+        _k1 = [1,0,192]
+        res = self.g.getGKeys(_k1)
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0], "G1")
+
+    def test_getLCDKeys_LCDK1_5(self):
+        for i in range(1,5):
+            _keystr = "LCDK{}".format(i)
+            res = self.g.getLCDKeys(self.lcdk[_keystr])
+            self.assertEqual(len(res), 1)
+            self.assertEqual(res[0], _keystr)
+
+    def test_getDevice_Raise(self):
+        with self.assertRaises(ValueError):
+            self.g.getDevice(0x0000,0x0000)
 
     def test_isLCDLightOn_True(self):
         self.assertTrue(self.g.isLCDLightOn(128))
